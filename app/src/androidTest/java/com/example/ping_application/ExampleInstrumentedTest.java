a361cc2cb849162a487c477d9af4082aaadbe867
SGO-Nova <51762752+SGO-Nova@users.noreply.github.com>
2021-03-28 05:40:02 -0500

    Login and second area for adding content

--- app/src/androidTest/java/com/example/ping_application/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/ping_application/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.ping;
+package com.example.ping_application;
 
 import android.content.Context;
 
@@ -21,7 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
-        assertEquals("com.example.ping", appContext.getPackageName());
+        assertEquals("com.example.ping_application", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/example/ping_application/ExampleUnitTest.java
+++ app/src/test/java/com/example/ping_application/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.ping;
+package com.example.ping_application;
 
 import org.junit.Test;
 
