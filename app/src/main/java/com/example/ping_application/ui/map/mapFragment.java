d1912d4d9901bf4a925e31e0dfad8c7c8333f09a
SGO-Nova <51762752+SGO-Nova@users.noreply.github.com>
2021-03-31 13:13:59 -0500

    map fragment

--- app/src/main/java/com/example/ping_application/ui/map/mapFragment.java
+++ app/src/main/java/com/example/ping_application/ui/map/mapFragment.java
@@ -1,4 +1,4 @@
-package com.example.ping_application.ui.home;
+package com.example.ping_application.ui.map;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,15 +11,15 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.ping_application.R;
 
-public class HomeFragment extends Fragment {
+public class mapFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private mapViewModel mapViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        mapViewModel =
+                new ViewModelProvider(this).get(mapViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
         return root;
     }
 }
---
+++ app/src/main/java/com/example/ping_application/ui/map/mapViewModel.java
@@ -0,0 +1,12 @@
+package com.example.ping_application.ui.map;
+
+import androidx.lifecycle.ViewModel;
+
+public class mapViewModel extends ViewModel {
+
+
+    public mapViewModel() {
+
+    }
+
+}
