3ed7c6a09d42d02415a7935c8fae6e61f350c4b4
SGO-Nova <51762752+SGO-Nova@users.noreply.github.com>
2021-04-24 22:36:51 -0500

    Added fragment to hold contacts info, will be edited later

---
+++ app/src/main/java/com/example/ping_application/AddedFragment.java
@@ -0,0 +1,11 @@
+package com.example.ping_application;
+
+import androidx.fragment.app.Fragment;
+
+
+public class AddedFragment extends Fragment{
+    public AddedFragment(){
+        super(R.layout.fragment_added);
+    }
+
+}
