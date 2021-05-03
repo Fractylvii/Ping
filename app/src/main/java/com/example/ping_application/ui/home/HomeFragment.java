ca871760ff65f3d3f5c021089a7cb575f3f1ab7e
SGO-Nova <51762752+SGO-Nova@users.noreply.github.com>
2021-05-03 12:54:57 -0500

    Going out button on home

--- app/src/main/java/com/example/ping_application/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ping_application/ui/home/HomeFragment.java
@@ -1,15 +1,18 @@
 package com.example.ping_application.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.ping_application.R;
+import com.example.ping_application.ui.bigbutton.BigButtonFragment;
 
 public class HomeFragment extends Fragment {
 
@@ -20,6 +23,15 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button map = (Button) root.findViewById(R.id.goingOutButton);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), BigButtonFragment.class));
+            }
+        });
+
         return root;
     }
 }
